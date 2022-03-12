4490dabbe6d81bcb89ec1f3392a2706e72ff516d
dalto <dalto@fastmail.com>
2022-03-12 15:58:09 -0600

    [fstab] Fix empty UUID detection

--- calamares-extra-modules/src/modules/fstab/main.py
+++ calamares-extra-modules/src/modules/fstab/main.py
@@ -248,7 +248,7 @@ class FstabGenerator(object):
 
         if has_luks:
             device = "/dev/mapper/" + partition["luksMapperName"]
-        elif partition["uuid"] is not None:
+        elif partition["uuid"]:
             device = "UUID=" + partition["uuid"]
         else:
             device = partition["device"]
